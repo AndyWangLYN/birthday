862f6ccd0d26f28f6de4de160224706cd28a9803
Heyu Wang <90009289+AndyWangLYN@users.noreply.github.com>
2023-05-29 20:51:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,10 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "的朋友张亦唯": "./imgs/zyw.png"
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "的朋友张亦唯": "./imgs/zyw.png",
+        "今天是你的生日": "./imgs/zyw.png",
+        "你要天天开心!": "./imgs/zyw.png"
+        "永远18岁❤️": "./imgs/zyw.png",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
